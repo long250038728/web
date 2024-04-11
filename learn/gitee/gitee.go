cbaf90af2f28c49dcfd246ccd71fe8ce880173eb
linlong <linl@avicks.com>
2024-04-11 11:16:37 +0800

    优化gitee

--- learn/gitee/gitee.go
+++ learn/gitee/gitee.go
@@ -17,17 +17,19 @@ type feature struct {
 }
 
 type features struct {
-	Token    string
-	Features []*feature
-	Kobe     []string
+	JenkinsToken string
+	GiteeToken   string
+	Features     []*feature
+	Kobe         []string
 }
 
 type Pr struct {
-	Token string
+	GiteeToken   string
+	JenkinsToken string
 }
 
-func NewPrGen(token string) *Pr {
-	return &Pr{token}
+func NewPrGen(giteeToken, jenkinsToken string) *Pr {
+	return &Pr{GiteeToken: giteeToken, JenkinsToken: jenkinsToken}
 }
 
 func (g *Pr) GenFeature(address []string, source, target string) ([]byte, error) {
@@ -38,7 +40,7 @@ func (g *Pr) GenFeature(address []string, source, target string) ([]byte, error)
 	for _, addr := range address {
 		list = append(list, &feature{Addr: addr, Source: source, Feature: target})
 	}
-	return (&gen.Impl{Name: "gen feature", TmplPath: "./gitee_feature.tmpl", Data: &features{Features: list, Token: g.Token}, IsFormat: false}).Gen()
+	return (&gen.Impl{Name: "gen feature", TmplPath: "./gitee_feature.tmpl", Data: &features{Features: list, GiteeToken: g.GiteeToken}, IsFormat: false}).Gen()
 }
 
 func (g *Pr) GenPrCreate(address []string, source, target string) ([]byte, error) {
@@ -49,7 +51,7 @@ func (g *Pr) GenPrCreate(address []string, source, target string) ([]byte, error
 	for _, addr := range address {
 		list = append(list, &feature{Addr: addr, Source: source, Feature: target})
 	}
-	return (&gen.Impl{Name: "gen pr create", TmplPath: "./gitee_pr.tmpl", Data: &features{Features: list, Token: g.Token}, IsFormat: false}).Gen()
+	return (&gen.Impl{Name: "gen pr create", TmplPath: "./gitee_pr.tmpl", Data: &features{Features: list, GiteeToken: g.GiteeToken}, IsFormat: false}).Gen()
 }
 
 func (g *Pr) GenMerge(address []string, kobe []string) ([]byte, error) {
@@ -62,10 +64,16 @@ func (g *Pr) GenMerge(address []string, kobe []string) ([]byte, error) {
 	}
 	return (&gen.Impl{Name: "gen pr merge", TmplPath: "./gitee_pr_merge.tmpl", Func: template.FuncMap{
 		"objectName": g.objectName,
-	}, Data: &features{Features: list, Token: g.Token, Kobe: kobe}, IsFormat: false}).Gen()
+		"name":       g.name,
+	}, Data: &features{Features: list, GiteeToken: g.GiteeToken, JenkinsToken: g.JenkinsToken, Kobe: kobe}, IsFormat: false}).Gen()
 }
 
 func (g *Pr) objectName(mainString, obj string) bool {
 	str := strings.ReplaceAll(mainString, "https://", "")
 	return strings.Split(str, "/")[2] == obj
+}
+
+func (g *Pr) name(mainString string) string {
+	str := strings.ReplaceAll(mainString, "https://", "")
+	return strings.Split(str, "/")[2]
 }
--- learn/gitee/gitee_test.go
+++ learn/gitee/gitee_test.go
@@ -1,47 +1,142 @@
 package gitee
 
+// https://gitee.com/api/v5/swagger#/postV5ReposOwnerRepoBranches
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/long250038728/web/tool/server/http"
 	"testing"
 )
 
 var token = "5f8aaa1e024cad5e24e86fda85c57f49"
+var jenkins = "admin:11fbfc1aab366147522f497f6c7d48b2ca"
 
-func TestFeature_Gen(t *testing.T) {
-	var address = []string{
-		"zhubaoe/socrates",
-		"zhubaoe/aristotle",
-		"zhubaoe/locke",
-		"fissiongeek/h5-sales",
-	}
-	var source = "master"
-	var target = "release/v3.5.60"
+var productList = []string{
+	//"zhubaoe/socrates",
+	"zhubaoe/aristotle",
+	//"zhubaoe/locke",
+	//"fissiongeek/h5-sales",
+}
+var createSource = "hotfix/reshapre_20240410"
+var createTarget = "master"
 
-	gen, err := NewPrGen(token).GenFeature(address, source, target)
+//============================template curl=========================
+
+// 创建分支
+func TestCreateFeatureCurl_Gen(t *testing.T) {
+	gen, err := NewPrGen(token, jenkins).GenFeature(productList, createSource, createTarget)
 	if err != nil {
 		return
 	}
 	fmt.Println(string(gen))
 }
 
-func TestPr_Gen(t *testing.T) {
-	var address = []string{
-		"zhubaoe/socrates",
-		"zhubaoe/aristotle",
-		"zhubaoe/locke",
-		"fissiongeek/h5-sales",
-		"zhubaoe-go/kobe",
-	}
-	var source = "feature/sm0501"
-	var target = "master"
-
-	gen, err := NewPrGen(token).GenPrCreate(address, source, target)
+// 创建PR
+func TestCreatePR_Gen(t *testing.T) {
+	gen, err := NewPrGen(token, jenkins).GenPrCreate(productList, createSource, createTarget)
 	if err != nil {
 		return
 	}
 	fmt.Println(string(gen))
 }
 
+// ==============================http=======================
+type PrListItem struct {
+	HtmlUrl string `json:"html_url"`
+}
+
+// 创建分支
+func TestCreateFeature_Http(t *testing.T) {
+	for _, addr := range productList {
+		client := http.NewClient()
+		data := map[string]any{
+			"access_token": token,
+			"refs":         createSource,
+			"branch_name":  createTarget,
+		}
+		_, _, err := client.Post(context.Background(), fmt.Sprintf("https://gitee.com/api/v5/repos/%s/branches", addr), data)
+		if err != nil {
+			t.Error(addr, err)
+			continue
+		}
+		t.Log(addr, "ok")
+	}
+}
+
+// 创建PR
+func TestCreatePR_Http(t *testing.T) {
+	pr := make([]string, 0, len(productList))
+
+	for _, addr := range productList {
+		client := http.NewClient()
+		data := map[string]any{
+			"access_token": token,
+			"title":        fmt.Sprintf("%s merge %s", createTarget, createSource),
+			"head":         createSource,
+			"base":         createTarget,
+		}
+		b, _, err := client.Post(context.Background(), fmt.Sprintf("https://gitee.com/api/v5/repos/%s/pulls", addr), data)
+		if err != nil {
+			t.Error(addr, err)
+			continue
+		}
+
+		var item *PrListItem
+		err = json.Unmarshal(b, &item)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		pr = append(pr, item.HtmlUrl)
+	}
+
+	for _, p := range pr {
+		fmt.Println(p)
+	}
+}
+
+// 获取pr地址
+func TestGetPrAddr_Http(t *testing.T) {
+	pr := make([]string, 0, len(productList))
+
+	for _, addr := range productList {
+		url := fmt.Sprintf("https://gitee.com/api/v5/repos/%s/pulls?access_token=%s&state=all&head=%s&base=%s&sort=created&direction=desc&page=1&per_page=20",
+			addr, token, createSource, createTarget)
+		client := http.NewClient()
+		b, code, err := client.Get(context.Background(), url, nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if code != 200 {
+			t.Error(errors.New("request code is error"))
+			return
+		}
+
+		var list []*PrListItem
+		err = json.Unmarshal(b, &list)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if len(list) != 1 {
+			t.Error(errors.New("list num is not one"))
+			return
+		}
+
+		pr = append(pr, list[0].HtmlUrl)
+	}
+
+	for _, p := range pr {
+		fmt.Println(p)
+	}
+}
+
+//============================上线流程生产=========================
+
+// 上线流程
 func TestPrMerge_Gen(t *testing.T) {
 	var address = []string{
 		"https://gitee.com/zhubaoe/locke/pulls/365",
@@ -54,7 +149,7 @@ func TestPrMerge_Gen(t *testing.T) {
 		"kobe-stock",
 		"kobe-customer",
 	}
-	gen, err := NewPrGen(token).GenMerge(address, kobe)
+	gen, err := NewPrGen(token, jenkins).GenMerge(address, kobe)
 	if err != nil {
 		fmt.Println(err)
 		return
