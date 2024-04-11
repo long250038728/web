bb93dc7948a78399e226bb29edf14a3f842b2e01
linlong <linl@avicks.com>
2024-04-11 14:38:43 +0800

    gitee 添加企业微信提醒

--- learn/gitee/gitee_test.go
+++ learn/gitee/gitee_test.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/long250038728/web/tool/server/http"
+	"strings"
 	"testing"
 )
 
@@ -16,31 +17,25 @@ var jenkins = "admin:11fbfc1aab366147522f497f6c7d48b2ca"
 var productList = []string{
 	//"zhubaoe/socrates",
 	"zhubaoe/aristotle",
-	//"zhubaoe/locke",
-	//"fissiongeek/h5-sales",
-}
-var createSource = "hotfix/reshapre_20240410"
-var createTarget = "master"
-
-//============================template curl=========================
-
-// 创建分支
-func TestCreateFeatureCurl_Gen(t *testing.T) {
-	gen, err := NewPrGen(token, jenkins).GenFeature(productList, createSource, createTarget)
-	if err != nil {
-		return
-	}
-	fmt.Println(string(gen))
+	"zhubaoe/locke",
+	"fissiongeek/h5-sales",
 }
 
-// 创建PR
-func TestCreatePR_Gen(t *testing.T) {
-	gen, err := NewPrGen(token, jenkins).GenPrCreate(productList, createSource, createTarget)
-	if err != nil {
-		return
-	}
-	fmt.Println(string(gen))
-}
+// 创建feature/release分支 （创建分支）
+var createSource = "master"
+var createTarget = "release/v3.5.53"
+
+// //提pr （创建pr）
+//var createSource = "feature/sm0403"
+//var createTarget = "release/v3.5.53"
+
+////合到check （创建pr）
+//var createSource = "release/v3.5.53"
+//var createTarget = "check"
+
+////合到master （创建pr）
+//var createSource = "release/v3.5.53"
+//var createTarget = "master"
 
 // ==============================http=======================
 type PrListItem struct {
@@ -68,9 +63,9 @@ func TestCreateFeature_Http(t *testing.T) {
 // 创建PR
 func TestCreatePR_Http(t *testing.T) {
 	pr := make([]string, 0, len(productList))
+	client := http.NewClient()
 
 	for _, addr := range productList {
-		client := http.NewClient()
 		data := map[string]any{
 			"access_token": token,
 			"title":        fmt.Sprintf("%s merge %s", createTarget, createSource),
@@ -92,9 +87,16 @@ func TestCreatePR_Http(t *testing.T) {
 		pr = append(pr, item.HtmlUrl)
 	}
 
-	for _, p := range pr {
-		fmt.Println(p)
+	text := "PR 创建成功: \n\n" + strings.Join(pr, "\n") + "\n\n"
+	data := map[string]any{
+		"msgtype": "text",
+		"text": map[string]any{
+			"content":               text,
+			"mentioned_mobile_list": []string{"18575538087"},
+		},
 	}
+	b, code, err := client.Post(context.Background(), "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=991cbde3-6963-4adc-a25c-7a6402ab7d38", data)
+	t.Log(string(b), code, err)
 }
 
 // 获取pr地址
@@ -132,6 +134,26 @@ func TestGetPrAddr_Http(t *testing.T) {
 	for _, p := range pr {
 		fmt.Println(p)
 	}
+}
+
+//============================template curl=========================
+
+// 创建分支
+func TestCreateFeatureCurl_Gen(t *testing.T) {
+	gen, err := NewPrGen(token, jenkins).GenFeature(productList, createSource, createTarget)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(gen))
+}
+
+// 创建PR
+func TestCreatePR_Gen(t *testing.T) {
+	gen, err := NewPrGen(token, jenkins).GenPrCreate(productList, createSource, createTarget)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(gen))
 }
 
 //============================上线流程生产=========================
