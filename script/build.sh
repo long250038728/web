79d2e45be3abbb69add96b7134812ba2201a2af2
linlong <linl@avicks.com>
2024-05-20 14:46:36 +0800

    优化docker 打包

--- script/build.sh
+++ script/build.sh
@@ -5,16 +5,24 @@ cp -R ./../application  app/application
 cp -R ./../config  app/config
 cp -R ./../tool  app/tool
 cp -R ./../protoc  app/protoc
-cp -R ./../go*  app/
+cp -R ./../go.mod  app/
+cp -R ./../go.sum  app/
+
+# 根据服务名 构建用哪个dockerfile 打包成什么tag 暴露的端口号是多少
+DOCKER_FILE="./user_dockerfile"
+IMAGE_NAME="ccr.ccs.tencentyun.com/zhubaoe/user:latest"
+CONTAINER_NAME="user_docker"
+HTTP_PORT="8001:8001"
+GRPC_PORT="9001:9001"
 
 #打包成镜像
-docker build -t   ccr.ccs.tencentyun.com/zhubaoe/user:latest .
+docker build -f ${DOCKER_FILE} -t ${IMAGE_NAME} .
 
 #无需使用进行删除
 rm -rf  app
 
 # 上传到hub
-#docker push  ccr.ccs.tencentyun.com/zhubaoe/kobe-new:latest
+docker push ${IMAGE_NAME}
 
-
-docker run -it --name user --network my-service-network cr.ccs.tencentyun.com/zhubaoe/user:latest
+# docker 运行
+docker run  -itd  --network my-service-network  -p ${HTTP_PORT}  -p ${GRPC_PORT}  --name ${CONTAINER_NAME} ${IMAGE_NAME}
