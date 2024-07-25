f0a9781ceb437d04693997504ff1c83e09b31bf4
林龙 <linl@avicks.com>
2024-07-25 10:54:54 +0800

    分表

--- tool/persistence/orm/merge_test.go
+++ tool/persistence/orm/merge_test.go
@@ -102,19 +102,19 @@ func TestOnlineMerge2(t *testing.T) {
 }
 
 func TestStockChange1(t *testing.T) {
-	//configurator.NewYaml().MustLoad("/Users/linlong/Desktop/web/config/online/db.yaml", &config)
-	//db, err := NewGorm(&config)
-	//if err != nil {
-	//	t.Error(err)
-	//	return
-	//}
+	configurator.NewYaml().MustLoad("/Users/linlong/Desktop/web/config/online/db.yaml", &config)
+	db, err := NewGorm(&config)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	startMerchantId := 1
 	endMerchantId := 1000
 	sourceTable := "zby_stock_change_log"
 	mergeTable := "zby_stock_change_log_part_1"
 
-	startId := 1
-	maxId := 145759988
+	startId := 135230001
+	maxId := 146180000
 
 	batchNum := 10000
 
@@ -127,29 +127,31 @@ func TestStockChange1(t *testing.T) {
 		}
 
 		sql := fmt.Sprintf("INSERT INTO %s SELECT * FROM %s WHERE  merchant_id between %d and %d and id between %d and %d", mergeTable, sourceTable, startMerchantId, endMerchantId, s, e)
-		fmt.Println(sql)
 
-		//if err := db.Exec(sql).Error; err != nil {
-		//	fmt.Println(err)
-		//	return
-		//}
+		if err := db.Exec(sql).Error; err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(e)
+		time.Sleep(time.Millisecond * 500)
 	}
 }
 
 func TestStockChange2(t *testing.T) {
-	//configurator.NewYaml().MustLoad("/Users/linlong/Desktop/web/config/online/db.yaml", &config)
-	//db, err := NewGorm(&config)
-	//if err != nil {
-	//	t.Error(err)
-	//	return
-	//}
+	configurator.NewYaml().MustLoad("/Users/linlong/Desktop/web/config/online/db.yaml", &config)
+	db, err := NewGorm(&config)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	startMerchantId := 1001
 	endMerchantId := 2000
 	sourceTable := "zby_stock_change_log"
 	mergeTable := "zby_stock_change_log_part_2"
 
-	startId := 1
-	maxId := 145759988
+	startId := 96000001
+	maxId := 146180000
 
 	batchNum := 10000
 
@@ -162,12 +164,14 @@ func TestStockChange2(t *testing.T) {
 		}
 
 		sql := fmt.Sprintf("INSERT INTO %s SELECT * FROM %s WHERE  merchant_id between %d and %d and id between %d and %d", mergeTable, sourceTable, startMerchantId, endMerchantId, s, e)
-		fmt.Println(sql)
 
-		//if err := db.Exec(sql).Error; err != nil {
-		//	fmt.Println(err)
-		//	return
-		//}
+		if err := db.Exec(sql).Error; err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(e)
+		time.Sleep(time.Millisecond * 500)
 	}
 }
 
@@ -185,7 +189,7 @@ func TestStockChange2(t *testing.T) {
 func TestMath(t *testing.T) {
 	a := int64(100)
 	b := int64(100)
-	if a >= math.MaxInt64-b {
+	if a > math.MaxInt64-b {
 		t.Log("溢出")
 	}
 	t.Log(a, b)
@@ -194,27 +198,27 @@ func TestMath(t *testing.T) {
 func TestSlices(t *testing.T) {
 	a := []string{"1", "2", "3", "4", "5", "6", "7"}
 
-	// b此时是从坐标2取到到坐标4(不包含4)，同时限制了长度为2，此时如果append，就会开辟一个新的空间，此时修改不会影响到a
-	// c此时是从坐标2取到到坐标4(不包含4)，此时不限制长度，此时如果append，发现底层a数组无需扩容，只是就会把append替换原先的值
+	// b此时是从坐标2取到到坐标4(不包含4)，同时限制了长度为2，此时如果append，就会开辟一个新的空间，此时修改不会影响到a（因为扩容了）
+	// c此时是从坐标2取到到坐标4(不包含4)，此时不限制长度，此时如果append，发现底层a数组无需扩容，只是就会把append替换原先的值，如果需要扩容就会开辟一个新的空间，此时修改不会影响到a
 	b := a[2:4:4]
 	b = append(b, "6")
 	b[1] = "aaa"
 	c := a[2:4]
 	c = append(c, "append")
 
-	// 使用copy 找最小的 (是长度而不是容量，如果长度为0时copy后为空)
+	// 使用copy 找最小的长度 (是长度而不是容量，如果长度为0时copy后为依旧为空)
 	d := make([]string, 2, 2)
 	copy(d, a)
 
-	//由于切割是在原有的数组上面操作，所以可能会内存泄露(如果数组a达到了1G，此时a不会被销毁，因为被一直引用)
-	//可以用copy 或 新增一个新的数组
+	//由于切割是指针指向原有的数组上面操作，所以可能会内存泄露(如果数组a有1G，此时a退出作用域应该被销毁，但是切割引用了a，导致a无法被销毁)
+	//可以用copy 或 新增一个新的数组append
 	e := a[:2]                     //内存泄露
 	f := make([]string, 0, len(a)) //解决内存泄露
 	f = append(f, a...)
 
 	//空数组及数组长度为空
-	//所以一般不使用  f == nil (true)      g == nil (false)
-	//应该使用len(f)  len(g)
+	//所以一般不使用  g == nil (true)      h == nil (false)
+	//应该使用len(g)  len(h)
 	var g []string            //空数字 null
 	h := make([]string, 0, 0) //数组长度为空
 
@@ -222,5 +226,12 @@ func TestSlices(t *testing.T) {
 }
 
 func TestMap(t *testing.T) {
+	hash := make(map[string]string, 1)
+	t.Log(len(hash))
+}
 
+func TestString(t *testing.T) {
+}
+
+func TestChan(t *testing.T) {
 }
