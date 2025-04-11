5578a2080b6f97a4cfb3abade8d1d505fb03f6e6
林龙 <linl@avicks.com>
2025-04-11 13:37:09 +0800

    protoc

--- protoc/auth/authorized_grpc.pb.go
+++ protoc/auth/authorized_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v5.27.1
-// source: authXXX.proto
+// source: authorized.proto
 
 package auth
 
@@ -137,5 +137,5 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "authXXX.proto",
+	Metadata: "authorized.proto",
 }
