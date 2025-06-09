fecdfd7395c818e43c2c17060df2841aca576104
林龙 <linl@avicks.com>
2025-06-09 13:37:08 +0800

    script auth gen

--- script/script.sh
+++ script/script.sh
@@ -4,10 +4,10 @@
 # 2.进行dockerfile的docker打包
 # 3.docker image run / docker push / k8s
 
-## bash script.sh user /app/config  user:v1
-if [ $# -lt 3 ]; then
-    echo "错误：至少需要3个参数"
-    echo "用法: $0 服务名 config路径 镜像名"
+## bash script.sh user /app/config user:v1 web_my-service-network
+if [ $# -lt 4 ]; then
+    echo "错误：至少需要4个参数"
+    echo "用法: $0 服务名 config路径 镜像名 docker_NETWORK"
     exit 1
 fi
 
@@ -15,6 +15,7 @@ SCRIPT_DIR=$(dirname "$0")
 SERVER_NAME=$1
 CONFIG_PATH=$2
 IMAGE_NAME=$3
+DOCKER_NETWORK=$4
 BASE_DIR="${SCRIPT_DIR}/app"
 
 ## 创建根目录
@@ -37,7 +38,7 @@ docker build -f ./app/dockerfile  -t  $IMAGE_NAME .
 rm -rf  $BASE_DIR
 
 ## 运行
-docker run -itd --name $SERVER_NAME --network=my-service-network $IMAGE_NAME
+docker run -itd --name $SERVER_NAME --network=$DOCKER_NETWORK $IMAGE_NAME
 #kubectl set image deployment/"SERVER_NAME"-deployment test-container=$IMAGE_NAME
 
 
