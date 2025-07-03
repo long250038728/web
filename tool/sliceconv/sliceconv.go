edaacd11187cf07949655712a643cdf6b9c240cb
林龙 <linl@avicks.com>
2025-07-03 10:08:25 +0800

    泛型

--- tool/sliceconv/sliceconv.go
+++ tool/sliceconv/sliceconv.go
@@ -4,6 +4,70 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+//-------------------用于定义约定泛型的类型————————————
+
+// demo
+//
+// 1.定义了泛型支持的类型（int类型相关）
+// 2.必须实现Say接口
+// 3.同时组合demoInterface（即需要实现SayHello接口）
+// 4.同时组合comparable (可比较的)
+type demo interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+	Say()
+	demoInterface
+	comparable
+}
+
+type demoInterface interface {
+	SayHello()
+}
+
+//-------------------用于方法参数————————————————————
+
+// demo2Interface
+// 5.定义了接收T的接口
+type demo2Interface[T demo] interface {
+	SayHello2(T)
+}
+
+//-------------------
+
+type demoType[T demo] struct {
+	val1 T
+	val2 T
+	demo2Interface[T]
+}
+
+func (t *demoType[T]) Set(val1, val2 T) {
+	t.val1 = val1 // 1.定义了泛型支持的类型（int类型相关）
+	t.val2 = val2
+}
+
+func (t *demoType[T]) Do() {
+	t.val1.Say()        // 2.必须实现Say接口
+	t.val2.SayHello()   // 3.同时组合demoInterface（即需要实现SayHello接口）
+	_ = t.val1 > t.val2 // 4.comparable
+}
+
+func (t *demoType[T]) SayHello2(v T) {}
+
+//-------------------
+
+type sayHelloInt int32
+
+func (s sayHelloInt) Say()      {}
+func (s sayHelloInt) SayHello() {}
+
+func new() {
+	d := demoType[sayHelloInt]{}
+	d.Set(100, 2)
+	d.Do()
+	d.SayHello2(sayHelloInt(10))
+}
+
+//================================================================
+
 // IndexOf 查找元素是否在切片中存在(值通过闭包函数处理)
 func IndexOf[T any](slice []T, condition func(T) bool) (T, int) {
 	for index, item := range slice {
