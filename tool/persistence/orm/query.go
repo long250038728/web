92ea0010e496c7b3caa543c61293e7ebf51de120
林龙 <linl@avicks.com>
2025-09-08 14:46:14 +0800

    query

--- tool/persistence/orm/query.go
+++ tool/persistence/orm/query.go
@@ -2,6 +2,7 @@ package orm
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -44,12 +45,22 @@ func NewBoolQuery() *BoolQuery {
 }
 
 func (b *BoolQuery) Must(queries ...Query) *BoolQuery {
-	b.MustQueries = append(b.MustQueries, queries...)
+	for _, query := range queries {
+		if isNil(query) {
+			continue
+		}
+		b.MustQueries = append(b.MustQueries, query)
+	}
 	return b
 }
 
 func (b *BoolQuery) Should(queries ...Query) *BoolQuery {
-	b.ShouldQueries = append(b.ShouldQueries, queries...)
+	for _, query := range queries {
+		if isNil(query) {
+			continue
+		}
+		b.ShouldQueries = append(b.ShouldQueries, query)
+	}
 	return b
 }
 
@@ -82,38 +93,98 @@ func (b *BoolQuery) Do() (string, []interface{}) {
 	return strings.Join(parts, " AND "), args
 }
 
+func (b *BoolQuery) IsEmpty() bool {
+	return len(b.MustQueries) == 0 && len(b.ShouldQueries) == 0
+}
+
+// isNil 判断interface是否为空
+func isNil(i interface{}) bool {
+	if i == nil {
+		return true
+	}
+
+	vi := reflect.ValueOf(i)
+
+	//判断是否是指针，可通过指针判断指向的内存是不是为空
+	if vi.Kind() == reflect.Ptr {
+		return vi.IsNil()
+	}
+	//如果不是指针就代表一定有一个具体的值
+	return false
+}
+
 //==============================================================================================
 
-func Eq(field string, value interface{}) *Condition {
+func Eq(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: "=", Value: value}
 }
 
-func Neq(field string, value interface{}) *Condition {
+func Neq(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: "<>", Value: value}
 }
 
-func Gt(field string, value interface{}) *Condition {
+func Gt(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: ">", Value: value}
 }
 
-func Lt(field string, value interface{}) *Condition {
+func Lt(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: "<", Value: value}
 }
 
-func Gte(field string, value interface{}) *Condition {
+func Gte(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: ">=", Value: value}
 }
 
-func Lte(field string, value interface{}) *Condition {
+func Lte(field string, value interface{}, opts ...Opt) *Condition {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(value) {
+		return nil
+	}
+
 	return &Condition{Field: field, Op: "<=", Value: value}
 }
 
-func In(field string, values ...interface{}) Query {
+func In(field string, values interface{}, opts ...Opt) Query {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(values) {
+		return nil
+	}
+
 	sql := fmt.Sprintf("%s IN (?)", field)
-	return &rawQuery{sql: sql, args: values}
+	return &rawQuery{sql: sql, args: []any{values}}
 }
 
-func Between(field string, val, val2 interface{}) Query {
+func Between(field string, val, val2 interface{}, opts ...Opt) Query {
+	setting := NewSetting(opts...)
+	if !setting.allowIsZero && isZero(val) {
+		return nil
+	}
+
 	sql := fmt.Sprintf("%s between ? and ?", field)
 	return &rawQuery{sql: sql, args: []any{val, val2}}
 }
@@ -122,4 +193,48 @@ func Raw(sql string, values ...interface{}) Query {
 	return &rawQuery{sql: sql, args: values}
 }
 
+func isZero(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+	v := reflect.ValueOf(value)
+	switch v.Kind() {
+	case reflect.String:
+		return v.Len() == 0
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Slice, reflect.Array, reflect.Map, reflect.Chan:
+		return v.Len() == 0
+	case reflect.Ptr, reflect.Interface:
+		return v.IsNil()
+	}
+	return false
+}
+
 //==============================================================================================
+
+type Setting struct {
+	allowIsZero bool
+}
+
+func NewSetting(opts ...Opt) *Setting {
+	setting := &Setting{}
+	for _, opt := range opts {
+		opt(setting)
+	}
+	return setting
+}
+
+type Opt func(s *Setting)
+
+func AllowIsZero(allowIsZero bool) Opt {
+	return func(s *Setting) {
+		s.allowIsZero = allowIsZero
+	}
+}
--- tool/persistence/orm/query_test.go
+++ tool/persistence/orm/query_test.go
@@ -4,20 +4,24 @@ import "testing"
 
 func TestQuery(t *testing.T) {
 	query := NewBoolQuery().Must(
-		Eq("merchant_id", 100),
+		Eq("merchant_id", 0, AllowIsZero(true)),
 		Neq("merchant_shop_id", 200),
 		Gt("goods_id", 2000),
-
 		NewBoolQuery().Should(
 			NewBoolQuery().Must(Gte("status", 1), Lt("status", 2)),
 			NewBoolQuery().Must(Lte("status", 3)),
 			NewBoolQuery().Must(Between("status", 4, 5)),
 		),
-
-		In("order_id", []int32{1, 2, 3, 4, 5, 6}),
-		Raw("type = ?", 78),
+		In("order_id", []int32{1, 2, 3, 4, 5}),
+		Raw("type = ?", 7),
 	)
+
+	if query.IsEmpty() {
+		t.Log("is empty")
+		return
+	}
+
 	sql, args := query.Do()
 	t.Log(sql)
-	t.Log(args)
+	t.Log(args...)
 }
