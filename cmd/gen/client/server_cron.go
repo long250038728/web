8b1d9b6ae06fbb2751aac9461bcb8e2b9e0c2fb8
林龙 <linl@avicks.com>
2026-01-16 14:53:22 +0800

    cmd gen server

--- cmd/gen/client/server_cron.go
+++ cmd/gen/client/server_cron.go
@@ -13,6 +13,7 @@ import (
 
 type serverValue struct {
 	Server string `json:"server" yaml:"server"`
+	Domain string `json:"domain" yaml:"domain"`
 	Page   string `json:"page" yaml:"page"`
 	Protoc string `json:"protoc" yaml:"protoc"`
 }
@@ -128,8 +129,8 @@ func NewServerCornCorn() *ServerCorn {
 }
 
 func (c *ServerCorn) Server() *cobra.Command {
-	//go run main.go serverGen test /Users/linlong/Desktop/web/application
-	//go run main.go serverGen test /Users/linlong/Desktop/web/application github.com/long250038728/web application protoc
+	//go run main.go server test /Users/linlong/Desktop/web/application
+	//go run main.go server test /Users/linlong/Desktop/web/application github.com/long250038728/web application protoc
 	return &cobra.Command{
 		Use:   "server [服务名] [输出路径] [module-path:默认github.com/long250038728/web] [项目相对路径:默认application] [proto相对路径:默认protoc]",
 		Short: "创建server： 请输入 [服务名] [输出路径] [module-path:默认github.com/long250038728/web] [项目相对路径:默认application] [proto相对路径:默认protoc]",
@@ -137,24 +138,23 @@ func (c *ServerCorn) Server() *cobra.Command {
 		Args:  cobra.MinimumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 
-			server := args[0]
-			if len(args) >= 2 {
-				c.path = args[1]
-			}
-
 			modulePath := "github.com/long250038728/web"
 			application := "application"
 			protoc := "protoc"
 
-			if len(args) >= 3 {
-				modulePath = args[2]
-			}
 			if len(args) >= 4 {
-				application = args[3]
+				modulePath = args[3]
 			}
 			if len(args) >= 5 {
-				protoc = args[4]
+				application = args[4]
 			}
+			if len(args) >= 6 {
+				protoc = args[5]
+			}
+
+			serverName := args[0]
+			domainName := args[1]
+			c.path = args[2]
 			c.page = filepath.Join(modulePath, application)
 			c.protoc = filepath.Join(modulePath, protoc)
 
@@ -164,16 +164,20 @@ func (c *ServerCorn) Server() *cobra.Command {
 				//mkdir path
 				paths := []string{
 					filepath.Join(c.path),
-					filepath.Join(c.path, server),
-					filepath.Join(c.path, server, "cmd"),
-					filepath.Join(c.path, server, "cmd", "server"),
-					filepath.Join(c.path, server, "internal"),
-					filepath.Join(c.path, server, "internal", "domain"),
-					filepath.Join(c.path, server, "internal", "model"),
-					filepath.Join(c.path, server, "internal", "repository"),
-					filepath.Join(c.path, server, "internal", "handles"),
-					filepath.Join(c.path, server, "internal", "service"),
-					filepath.Join(c.path, server, "internal", "validate"),
+					filepath.Join(c.path, serverName),
+					filepath.Join(c.path, serverName, "cmd"),
+					filepath.Join(c.path, serverName, "cmd", "server"),
+
+					filepath.Join(c.path, serverName, "internal"),
+					filepath.Join(c.path, serverName, "internal", "handles"),
+					filepath.Join(c.path, serverName, "internal", "service"),
+					//filepath.Join(c.path, serverName, "internal", "validate"),
+
+					filepath.Join(c.path, serverName, "internal", "biz"),
+					filepath.Join(c.path, serverName, "internal", "biz", domainName),
+					filepath.Join(c.path, serverName, "internal", "biz", domainName, "domain"),
+					filepath.Join(c.path, serverName, "internal", "biz", domainName, "model"),
+					filepath.Join(c.path, serverName, "internal", "biz", domainName, "repository"),
 				}
 				for _, path := range paths {
 					_, err = os.Stat(path)
@@ -193,7 +197,7 @@ func (c *ServerCorn) Server() *cobra.Command {
 				var domainBytes []byte
 				var repositoryBytes []byte
 
-				v := &serverValue{Server: server, Page: c.page, Protoc: c.protoc}
+				v := &serverValue{Server: serverName, Domain: domainName, Page: c.page, Protoc: c.protoc}
 
 				if mainBytes, err = g.genMain(v); err != nil {
 					return err
@@ -215,22 +219,22 @@ func (c *ServerCorn) Server() *cobra.Command {
 				}
 
 				//write file
-				if err := os.WriteFile(filepath.Join(c.path, server, "cmd", "main.go"), mainBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "cmd", "main.go"), mainBytes, os.ModePerm); err != nil {
 					return err
 				}
-				if err := os.WriteFile(filepath.Join(c.path, server, "cmd", "server", "server.go"), serverBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "cmd", "server", "server.go"), serverBytes, os.ModePerm); err != nil {
 					return err
 				}
-				if err := os.WriteFile(filepath.Join(c.path, server, "internal", "handles", "handles.go"), handlesBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "internal", "handles", "handles.go"), handlesBytes, os.ModePerm); err != nil {
 					return err
 				}
-				if err := os.WriteFile(filepath.Join(c.path, server, "internal", "service", server+".go"), serviceBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "internal", "service", serverName+".go"), serviceBytes, os.ModePerm); err != nil {
 					return err
 				}
-				if err := os.WriteFile(filepath.Join(c.path, server, "internal", "domain", server+".go"), domainBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "internal", "biz", domainName, "domain", domainName+".go"), domainBytes, os.ModePerm); err != nil {
 					return err
 				}
-				if err := os.WriteFile(filepath.Join(c.path, server, "internal", "repository", server+".go"), repositoryBytes, os.ModePerm); err != nil {
+				if err := os.WriteFile(filepath.Join(c.path, serverName, "internal", "biz", domainName, "repository", domainName+".go"), repositoryBytes, os.ModePerm); err != nil {
 					return err
 				}
 
